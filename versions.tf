e16715e44fd20a1ee3af8467a870db3a1999b5b3
Alan Nix <alan.nix@lacework.net>
2021-07-13 09:42:41 -0400

    fix: updating to proper `azuread` provider version
    The `display_name` variable was introduced in v1.2 and `name` will be removed in v2.0 of the `azuread` provider.
    
    https://github.com/hashicorp/terraform-provider-azuread/releases/tag/v1.2.0

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.12.31"
 
   required_providers {
-    azuread = ">= 0.11"
+    azuread = "~> 1.2"
     azurerm = "~> 2.26"
     lacework = {
       source  = "lacework/lacework"
