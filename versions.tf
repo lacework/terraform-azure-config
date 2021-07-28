2f3e9d96947d60f8ec38486aeeb8ecc1b3158f69
Marcos Garcia <1045276+marcosgm@users.noreply.github.com>
2021-07-28 15:11:48 -0400

    fix(deps): upgrade min azurerm version to ~> 2.28
    Fix bug #17

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   required_providers {
     azuread = "~> 1.2"
-    azurerm = "~> 2.26"
+    azurerm = "~> 2.28"
     lacework = {
       source  = "lacework/lacework"
       version = "~> 0.3"
