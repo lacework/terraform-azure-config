25049d09584bcad912d5a024753edb3264890304
Marc Garcia <1045276+marcosgm@users.noreply.github.com>
2021-10-19 09:04:57 -0400

    fix(wait) better comment fix LW API 400 error when replacing configs too quick, i.e. when upgrading to v1.0

--- main.tf
+++ main.tf
@@ -48,9 +48,9 @@ resource "time_sleep" "wait_time" {
   create_duration = var.wait_time
   depends_on      = [azurerm_role_assignment.grant_reader_role_to_subscriptions]
   triggers = {
-    # Save the time we first heard about the application ID, so we wait again if it changes
+    # If App ID changes, trigger a wait between lacework_integration_azure_cfg destroys and re-creates, to avoid API errors
     app_id = local.application_id
-    # Same thing for LW integration name, which changed for v1.0, and waiting is needed to avoid API error when recreating it too fast
+    # If the Integration object changes (like during upgrade to v1.0), trigger a wait between lacework_integration_azure_cfg destroys and re-creates, to avoid API errors
     integration_name = var.lacework_integration_name
   }
 
